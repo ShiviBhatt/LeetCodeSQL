2c34dd0603cb081be76f66b2e8562f504dcc481c
Shivi Bhatt <in.shivi@gmail.com>
2021-11-02 16:30:09 -0400

    Update (Easy)1141. User Activity for the Past 30 Days I.sql

--- (Easy)1141. User Activity for the Past 30 Days I.sql
+++ (Easy)1141. User Activity for the Past 30 Days I.sql
@@ -1,1 +1,8 @@
 
+select 
+activity_date as day,
+count(distinct user_id) as active_users
+from activity
+where datediff(day, activity_date, '2019-07-27')<30
+group by activity_date
+having count(activity_type)>=1
