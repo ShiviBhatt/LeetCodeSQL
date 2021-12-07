b6bbb673cbad10692b457ec74bc774904f0325c6
Shivi Bhatt <in.shivi@gmail.com>
2021-12-06 23:51:55 -0500

    Create (Easy)197. Rising Temperature.sql

---
+++ Easy/(Easy)197. Rising Temperature.sql
@@ -0,0 +1,61 @@
+/*
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30)
+*/
+
+-- Solution
+
+WITH CTE AS
+(
+select id, recorddate, temperature,
+LAG(temperature,1) OVER (ORDER BY recorddate asc) prev_temp,
+LAG(recorddate,1) OVER (ORDER BY recorddate asc) pre_date
+from weather
+)
+
+select cte.id from cte
+where
+temperature > prev_temp and
+DATEDIFF(recorddate,pre_date)=1;
