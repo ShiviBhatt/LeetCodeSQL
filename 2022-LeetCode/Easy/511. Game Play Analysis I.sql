62ad2506ff86bbb938c00075efec6a51363029d1
Shivi Bhatt <in.shivi@gmail.com>
2022-07-15 15:56:00 -0400

    Create 511. Game Play Analysis I.sql

---
+++ 2022-LeetCode/Easy/511. Game Play Analysis I.sql
@@ -0,0 +1,17 @@
+
+select x.player_id, x.event_date as first_login
+from 
+(
+    select player_id, event_date, 
+    rank() over ( partition by player_id 
+                 order by event_date ) as rn
+    from activity
+) x
+where x.rn = 1
+
+
+
+OTHER SOLUTION
+
+
+select player_id, min(event_date) as first_login from activity group by player_id
